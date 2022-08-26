08782a1017c5e372373dce59e3664b16b6a0d085
martokk <martokk@martokk.martokk>
2022-08-26 16:55:46 -0500

    Setup.py

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-@
-requirements.txt,1/9/19359a61ae2446b51b549167b014da2fcf265768
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='workflow_manager',
-    version='22.08.16.4',
+    version='22.08.26.5',
     packages=find_packages(),
     url='',
     license='',
